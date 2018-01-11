d2f9b1d767217ba392d1fcbc08e5364338375602
Katrina Brinson <kbrinso@gmail.com>
2018-01-11 20:09:32 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board,index)
   if index.between?(1,9)
     if position_taken?(board,index)
-      return false
+      return true
     else 
       return !(position_taken?(board,index))
     end
